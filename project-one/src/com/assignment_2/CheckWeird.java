f75ff95c44d2f643e237bab411ec549342763ea9
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2021-04-20 20:33:57 +0200

    check in code updated

--- project-one/src/com/assignment_2/CheckWeird.java
+++ project-one/src/com/assignment_2/CheckWeird.java
@@ -4,7 +4,16 @@ public class CheckWeird {
 
 	public static void main(String[] args) {
 
-		
+		int x = 134;
+		int y= x%2 ;
+		if (y != 0)
+			System.out.println("Weird");
+		else if(x>=10 && x<=20)
+			System.out.println("Not Weird");
+		else if(x>=30 && x<=100)
+			System.out.println("Weird");
+		else if(x>=100)
+			System.out.println("Not Weird");
 	}
 
 }
--- project-one/src/com/assignment_2/MultiplicationTable.java
+++ project-one/src/com/assignment_2/MultiplicationTable.java
@@ -3,8 +3,12 @@ package com.assignment_2;
 public class MultiplicationTable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i, n = 2;
+		
+		for(i=1 ; i<11 ; i++)
+		{
+			System.out.println(n +" X "+ i +" = "+n*i);
+		}
 	}
 
 }
