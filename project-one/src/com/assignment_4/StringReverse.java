77a33ed48568fe7a27a68e8f6154040f081bdd84
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2021-05-04 13:54:00 +0200

    committed stringreverse program

---
+++ project-one/src/com/assignment_4/StringReverse.java
@@ -0,0 +1,16 @@
+package com.assignment_4;
+
+import java.util.Scanner;
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the string :");
+		String name = scan.next();
+		StringBuilder stringbuilder = new StringBuilder(name);
+
+		System.out.println("Reversed String : " + stringbuilder.reverse());
+	}
+
+}
