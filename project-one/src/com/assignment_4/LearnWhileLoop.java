86cb585df81ecc4344cbb2684f432968ece5a327
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2021-05-05 20:51:31 +0200

    LearnWhileloop checked in

---
+++ project-one/src/com/assignment_4/LearnWhileLoop.java
@@ -0,0 +1,27 @@
+package com.assignment_4;
+
+import java.util.Scanner;
+
+public class LearnWhileLoop {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the Number :");
+		int n;
+		do {
+			n = scan.nextInt();
+			if (n == 5) {
+				System.out.println("You are exited from the program");
+				break;
+			} else if (n < 11) {
+				System.out.println("Re-enter the number : ");
+				continue;
+			} else {
+				System.out.println("Wrong number");
+			}
+
+		} while (n < 11);
+		scan.close();
+	}
+
+}
