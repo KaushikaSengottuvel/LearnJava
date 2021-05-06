3a005f3a4096037700d469679c8bb8cb833b3642
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2021-05-06 19:48:18 +0200

    Error in code

---
+++ project-one/src/com/assignment_4/ArrayOfStringOperations.java
@@ -0,0 +1,34 @@
+package com.assignment_4;
+
+public class ArrayOfStringOperations {
+	public static StringBuilder sb = new StringBuilder();
+
+	public String processAnimals(String animals[]) {
+		String modified = "";
+		animals[2] = "Bear";
+		int i = 1;
+		for (String animal : animals) {
+			sb.append(i).append(".").append(animals);
+			i++;
+		}
+		System.out.println(sb.toString());
+		modified = sb.toString();
+		return modified;
+
+	}
+
+	public static void main(String[] args) {
+		ArrayOfStringOperations as = new ArrayOfStringOperations();
+		String[] animals = { "Caiman", "Caiman Lizard", "Cairn Terrier", "Camel", "Camel Spider", "Canaan Dog",
+				"Canadian Eskimo Dog", "Capybara", "Caracal", "Carolina Dog", "Carp", "Cassowary", "Cat", "Caterpillar",
+				"Catfish", "Cavador", "Cavalier King Charles Spaniel", "Cavapoo", "Centipede", "Cesky Fousek",
+				"Cesky Terrier", "Chameleon", "Chamois", "Cheagle", "Cheetah", "Chesapeake Bay Retriever", "Chicken",
+				"Chihuahua", "Chimaera", "Chimpanzee", "Chinchilla", "Chinese Crested Dog", "Chinese Paddlefish",
+				"Chinook", "Chinstrap Penguin", "Chipmunk", "Chipoo" };
+		// sb.append(animals);
+
+		String modified = as.processAnimals(animals);
+		System.out.println(modified);
+	}
+
+}
