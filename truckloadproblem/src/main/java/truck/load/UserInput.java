df76c1d00c6b38b2753ca4cee9dabc0bc90e0f99
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2021-05-11 14:25:52 +0200

    userinput for truckload

---
+++ truckloadproblem/src/main/java/truck/load/UserInput.java
@@ -0,0 +1,35 @@
+package truck.load;
+
+public class UserInput {
+
+	private int trucks;
+	private int boxes;
+	private int[] weights;
+
+	// Getters and Setters
+
+	public int getTrucks() {
+		return trucks;
+	}
+
+	public void setTrucks(int trucks) {
+		this.trucks = trucks;
+	}
+
+	public int getBoxes() {
+		return boxes;
+	}
+
+	public void setBoxes(int boxes) {
+		this.boxes = boxes;
+	}
+
+	public int[] getWeights() {
+		return weights;
+	}
+
+	public void setWeights(int[] weights) {
+		this.weights = weights;
+	}
+
+}
