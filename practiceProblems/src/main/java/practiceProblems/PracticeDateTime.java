f82c59f33f3a77655187dbf70dddf2df30532d08
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2021-05-13 11:48:35 +0200

    checkin practice date time class

---
+++ practiceProblems/src/main/java/practiceProblems/PracticeDateTime.java
@@ -0,0 +1,13 @@
+package practiceProblems;
+
+import java.time.LocalDate;
+
+public class PracticeDateTime {
+	
+	public LocalDate addDaysAndWeeks(LocalDate date)
+	{
+		return date.plusDays(5).plusWeeks(2);
+			
+	}
+
+}
