8511a37822909b385775dfcd45546a6dbae3bf8b
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2021-05-13 11:46:29 +0200

    checkin arraypractice problem

---
+++ practiceProblems/src/main/java/practiceProblems/PracticeArrayList.java
@@ -0,0 +1,15 @@
+package practiceProblems;
+
+import java.util.ArrayList;
+
+public class PracticeArrayList {
+	public String checkArray(ArrayList<String> animals)
+	{
+		if(animals.contains("Lion"))
+		{
+			return "Yayy";
+		}
+		else return "Oops";
+	}
+
+}
