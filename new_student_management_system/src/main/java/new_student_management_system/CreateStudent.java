f3acb57c7666abc625b8653018b4db7e71b34952
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2021-10-16 13:25:26 +0200

    new studetnt managemnt system main method created

---
+++ new_student_management_system/src/main/java/new_student_management_system/CreateStudent.java
@@ -0,0 +1,53 @@
+package new_student_management_system;
+
+import java.time.LocalDate;
+
+public class CreateStudent {
+
+	private String firstName;
+	private String lastName;
+	private int id;
+	private LocalDate dob;
+	private String emailId;
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+}
