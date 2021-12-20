a567d854adaecef8996cca64586b7d33c6e6f06b
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2021-12-20 18:00:56 +0100

    Java practice problems
    Display A_to_Z

---
+++ try_java_problems/src/main/java/try_java_problems/display_A_to_Z.java
@@ -0,0 +1,21 @@
+package try_java_problems;
+
+import java.util.Scanner;
+
+public class display_A_to_Z {
+
+	public static void main(String[] args) {
+		char a;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Alphabet :");
+		// a = sc.next().charAt(0);
+		/*
+		 * while (a != 'z' && a != 'Z') { System.out.println(a + " "); a++; }
+		 * System.out.println(a);
+		 */
+		for (a = sc.next().charAt(0); a <= 'Z' || a <= 'z'; a++) {
+			System.out.println(a);
+		}
+	}
+
+}
