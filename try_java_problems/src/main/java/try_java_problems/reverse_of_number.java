f8887ecac801666789938032812779175cab74a5
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2021-12-21 16:45:40 +0100

    Java practice problems
    Reverse of a number

---
+++ try_java_problems/src/main/java/try_java_problems/reverse_of_number.java
@@ -0,0 +1,22 @@
+package try_java_problems;
+
+import java.util.Scanner;
+
+public class reverse_of_number {
+
+	public static void main(String[] args) {
+		int a, b, c = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Integer :");
+		a = sc.nextInt();
+		while (a > 0) {
+			b = a % 10;
+			a = a / 10;
+			c = b + c * 10;
+
+		}
+		System.out.println("Reverse of the number :" + c);
+
+	}
+
+}
