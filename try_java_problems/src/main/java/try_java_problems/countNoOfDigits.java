ee52806dd46b00eee5611d41fdab4f0c2e89d886
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2021-12-30 18:20:50 +0100

    Java practice problems
    countNoOfDigits

---
+++ try_java_problems/src/main/java/try_java_problems/countNoOfDigits.java
@@ -0,0 +1,21 @@
+package try_java_problems;
+
+import java.util.Scanner;
+
+public class countNoOfDigits {
+
+	public static void main(String[] args) {
+		int a, count = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Integer :");
+		a = sc.nextInt();
+		while (a > 0) {
+			a = a / 10;
+			count++;
+		}
+		System.out.println(count);
+		sc.close();
+
+	}
+
+}
