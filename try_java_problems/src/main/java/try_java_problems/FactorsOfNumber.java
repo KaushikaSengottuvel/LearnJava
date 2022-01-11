86252772446df45638cb28531f0df36e625169fc
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2022-01-11 17:48:24 +0100

    Java Practice Problems
    Factors of a number

--- try_java_problems/src/main/java/try_java_problems/FactorsOfNumber.java
+++ try_java_problems/src/main/java/try_java_problems/FactorsOfNumber.java
@@ -6,15 +6,22 @@ public class FactorsOfNumber {
 
 	public static void main(String[] args) {
 		int a, start = 1;
+		boolean isPrime = true;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number :");
 		a = sc.nextInt();
 		sc.close();
-		System.out.println("The facors of the number " + a + " are ");
-		for (; start <= a / 2; start++) {
-			if (a % start == 0)
+		System.out.print("The factors of the number " + a + " are ");
+		for (; start <= a / 2; start++) { // it is enough to check half of the given number. so a/2
+			if (a % start == 0) {
 				System.out.print(start + " ");
+				isPrime = false;
+			}
 		}
+		if (isPrime == true)
+			System.out.print(a + ". Its a Prime Number.");
+		else
+			System.out.print(a);
 
 	}
 
