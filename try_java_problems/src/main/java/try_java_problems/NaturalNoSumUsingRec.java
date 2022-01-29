ae718c1c3db3105a8f3dda06588d41e246333f7f
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2022-01-29 19:18:22 +0100

    Java Practice Problems
    Sum of n Natural Numbers using Recursion

---
+++ try_java_problems/src/main/java/try_java_problems/NaturalNoSumUsingRec.java
@@ -0,0 +1,26 @@
+package try_java_problems;
+
+import java.util.Scanner;
+
+public class NaturalNoSumUsingRec {
+	int sum = 0;
+
+	public int findSum(int a) {
+
+		if (a > 1) {
+			sum = a + findSum(a - 1);
+		} else
+			return a;
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		int a, sum;
+		NaturalNoSumUsingRec obj = new NaturalNoSumUsingRec();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number :");
+		a = sc.nextInt();
+		System.out.println("Sum of " + a + " natural numbers are " + obj.findSum(a));
+	}
+
+}
