d13196c0f81076b7b8275a9d03f65e709b763dd9
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2022-02-07 18:26:49 +0100

    Create FoodMenu.java
    FoodMenu.java

---
+++ online_fastfood_ordering_project/src/main/java/online_fastfood_ordering_project/FoodMenu.java
@@ -0,0 +1,15 @@
+package online_fastfood_ordering_project;
+
+public class FoodMenu {
+	public void foodMenu() {
+		System.out.println("Your Menu Card");
+		System.out.println(
+				"ItemList                       Price in Rupees\n1. ==Burgers== \na) Chicken                       90\nb) Cheese                        60\nc) Vegan                         65");
+		System.out.println(
+				"\n2. ==Pizza== \na) Chicken                       110\nb) Cheesy Veg                    90\nc) Vegan                         95");
+		System.out.println(
+				"\n3. ==Chicken==\na)Fried Chicken                  90\nb) Cheesy Chicken fry            75\nc) Half Grilled                  150 \nd)Full Grilled                   290");
+		System.out.println(
+				"\n4. ==Drinks==\na) Water                         10\nb) Coke                          20\nc) Milkshakes                    65\nd) Cappucino                     35");
+	}
+}
