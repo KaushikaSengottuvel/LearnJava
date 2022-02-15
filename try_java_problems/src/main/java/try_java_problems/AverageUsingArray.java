bdb65ebda6c5db016c2d644a9ed0387c1fcf6a6e
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2022-02-15 18:00:30 +0100

    Create AverageUsingArray.java
    Find average using arrays with normal and enhanced for loop

---
+++ try_java_problems/src/main/java/try_java_problems/AverageUsingArray.java
@@ -0,0 +1,25 @@
+package try_java_problems;
+
+import java.util.Scanner;
+
+public class AverageUsingArray {
+
+	public static void main(String[] args) {
+		int n, avg1 = 0, avg2 = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the count of numbers :");
+		n = sc.nextInt();
+		double num[] = new double[n];
+		for (int i = 0; i < n; i++) {
+			num[i] = sc.nextDouble();
+			avg1 += num[i];
+		}
+		System.out.println("Average using normal for loop : " + avg1 / n);
+
+		for (double a : num) {
+			avg2 += a;
+		}
+		System.out.println("Using enhanced for loop : " + avg2 / n);
+	}
+
+}
