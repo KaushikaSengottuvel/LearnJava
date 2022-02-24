48bfafb2b72993676fc0ba576e6972918d51ea41
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2022-02-24 17:57:41 +0100

    Update CharacterFrequency.java

--- try_java_problems/src/main/java/java_string_problems/CharacterFrequency.java
+++ try_java_problems/src/main/java/java_string_problems/CharacterFrequency.java
@@ -6,16 +6,23 @@ public class CharacterFrequency {
 
 	public static void main(String[] args) {
 		String s;
-		char c;
+		char c, d;
 		int len, count = 0;
+		CharacterFrequency obj = new CharacterFrequency();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the String :");
 		s = sc.nextLine();
 		System.out.println("Enter the character to check the frequency for :");
 		c = sc.next().charAt(0);
+
+		// Character is a predefined wrapper class to manipulate characters.
+		c = Character.toLowerCase(c);
+
+		// Converting to upperCase using ASCII values
+		d = (char) (c - 32);
 		len = s.length();
 		for (int i = 0; i < len; i++) {
-			if (s.charAt(i) == c) {
+			if (s.charAt(i) == c || s.charAt(i) == d) {
 				++count;
 			}
 		}
