9a5a42086a832f656bd58fe285081d0b1db74429
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2022-03-04 15:47:58 +0100

    Create Pattern2.java

---
+++ try_java_problems/src/main/java/pattern_problems/Pattern2.java
@@ -0,0 +1,28 @@
+package pattern_problems;
+
+import java.util.Scanner;
+
+public class Pattern2 {
+
+	// 1
+	// 12
+	// 123
+	// 1234 right angled triangle
+
+	public static void main(String[] args) {
+		int n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the no of rows :");
+		n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+
+		sc.close();
+	}
+
+}
