2ad49d3925da9f37bf5d82f85f8c7719f85f4bb8
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2022-03-08 17:31:42 +0100

    Update Pattern6.java
    Full pyramid triangle using Star

--- try_java_problems/src/main/java/pattern_problems/Pattern6.java
+++ try_java_problems/src/main/java/pattern_problems/Pattern6.java
@@ -22,8 +22,8 @@ public class Pattern6 {
 			for (int i = 1; i < x; i++) {// for gap
 				System.out.print(" ");
 			}
-			for (int j = 0; j < a * 2 - 1; j += 2) {// fro printing star
-				System.out.print(" * ");
+			for (int j = 0; j < a * 2 - 1; j++) {// fro printing star
+				System.out.print("*");
 			}
 			System.out.println();// new line
 			x--;
