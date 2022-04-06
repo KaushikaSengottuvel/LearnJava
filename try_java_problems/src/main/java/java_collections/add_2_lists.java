7e1a4599250686e3eddc06d9a8893e9cf96014af
Kaushika Sengottuvel <kaushikasengottuvel@gmail.com>
2022-04-06 15:58:50 +0200

    Adding 2 Lists Using AddAll method

---
+++ try_java_problems/src/main/java/java_collections/add_2_lists.java
@@ -0,0 +1,24 @@
+package java_collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class add_2_lists {
+
+	public static void main(String[] args) {
+		List list1 = new ArrayList<>();// this List can hold all datatypes
+		List<Integer> list2 = new ArrayList<>();
+		list1.add("a");
+		list1.add("b");
+		list1.add("c");
+		list2.add(22);
+		list2.add(222);
+		list2.add(252);
+		System.out.println(list1);
+		// Using addAll method, you can add collections
+		list1.addAll(list2);
+		System.out.println(list1);
+
+	}
+
+}
